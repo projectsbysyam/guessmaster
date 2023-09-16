5658305af224e039c62e464807180921d5a18a8e
Anusree <anusreechandra.327@gmail.com>
2023-09-16 18:24:26 +0000

    adminapp customer(agent,dealer),change game

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,5 +4,10 @@ from django.urls import path
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('index',views.index,name="index")
+    path('index',views.index,name="index"),
+    path('agent',views.agent,name="agent"),
+    path('dealer',views.dealer,name="dealer"),
+    path('changegame',views.changegame,name="changegame"),
+
+
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 def index(request):
     return render(request,'adminapp/index.html')
 
+def agent(request):
+    return render(request,'adminapp/customer/agent.html')
+
+def dealer(request):
+    return render(request,'adminapp/customer/dealer.html')
+
+def changegame(request):
+    return render(request,'adminapp/changegame.html')
+
+
+
