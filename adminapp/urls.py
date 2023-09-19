526815126ca75eab025538933405ddd0f27cebb3
projectsbysyam <syamjithbabu.papaercorner@gmail.com>
2023-09-19 15:24:51 +0530

    view agents

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('index',views.index,name="index"),
     path('agent',views.agent,name="agent"),
     path('add-agent',views.add_agent,name="add_agent"),
-    path('dealer',views.dealer,name="dealer"),
+    path('view-agent',views.view_agent,name="view_agent"),
     path('change-game',views.change_game,name="changegame"),
     path('monitor',views.monitor,name="monitor"),
     path('results',views.results,name="results"),
     path('dailyreport',views.daily_report,name="dailyreport"),
-    path('changepassword',views.change_password,name="changepassword"),
+    path('change-password',views.change_password,name="change_password"),
 
 ]
